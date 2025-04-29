06ec620f06dc2d83c15353560a3aa79eb8a72b03
linco <linklinco@163.com>
2025-04-29 22:16:35 +0800

    新版blog生成器，增加生成静态密码功能

--- BlogMaker.py
+++ BlogMaker.py
@@ -1,5 +1,67 @@
 import os
 import yaml
+import time
+import json
+from datetime import datetime
+from lib.Cryption import Cryption
+
+class Article:
+    def __init__(self,filename):
+        # 根据文件名格式就是$name@topic%time#label  name、topic、time只能有一个 label无数个 如果以$开头表示加密        
+        if filename[0] == '$':
+            self.needPassword = True
+            filename = filename[1:]
+        else:
+            self.needPassword = False
+        # 从后往前分割
+        # 1. 先分割#号，分割出标签
+        if '#' in filename:
+            filename, *self.label = filename.split('#')
+        else:
+            self.label = []
+        # 2. 再分割%号，分割出时间
+        if '%' in filename:
+            filename, self.time = filename.split('%')
+        else:
+            self.time = None
+        # 3. 最后分割@号，分割出标题和话题
+        if '@' in filename:
+            self.title,self.topic = filename.split('@')
+        else:
+            self.title = filename
+            self.topic = None
+        self.text = ''  # 文章内容
+    
+    def generate(self,filePath,password='12345678'):
+        # 判断文件是否存在
+        if os.path.exists(filePath):
+            with open(filePath, 'r', encoding='utf8') as f:
+                if self.needPassword:
+                    # 需要加密
+                    self.text = Cryption.encrypt(password, f.read())
+                else:
+                    # 直接读取文件内容
+                    self.text = f.read()
+                if self.time is None:
+                    self.time = datetime.strftime(datetime.fromtimestamp(os.path.getmtime(filePath)), "%Y-%m-%d %H:%M:%S")
+        else:
+            raise FileNotFoundError(f"文件 {filePath} 不存在")
+    
+    def toJson(self):
+        # 将文章信息转换为json格式
+        data = {
+            'title': self.title,
+            'topic': self.topic,
+            'time': self.time,
+            'label': self.label,
+            'needPassword': self.needPassword,
+            'text': self.text
+        }
+        return json.dumps(data, ensure_ascii=False)
+    
+
+    def __str__(self):
+        return f"标题：{self.title}\n话题：{self.topic}\n时间：{self.time}\n标签：{" ".join(self.label)}\n加密：{self.needPassword}\n内容：{self.text}"
 
 
 class BlogMaker:
@@ -8,21 +70,35 @@ class BlogMaker:
             with open(os.path.join(config, "config.yaml"), "r", encoding='utf8') as f:
                 data = yaml.safe_load(f)
                 self.path = os.path.join(os.getcwd(),data['md-path'])
+                self.output = os.path.join(os.getcwd(), data['output-path'])
                 self.password = data['password']
         except:
             raise ValueError("配置文件无效,请核验")
-
     def genArticles(self):
-        for root, dirs, files in os.walk(self.path):
-            print(root)
-            print(dirs)
-            print(files)
-            # for file in files:
-            #     print(file)
+        try:
+            if not os.path.exists(self.path):
+                raise FileNotFoundError(f"md文件夹{self.path}不存在，请检查配置文件")
+            else:
+                for root, dirs, files in os.walk(self.path):
+                    for dir in dirs:
+                        os.mkdir(os.path.join(self.output, dir))
+                    for file in files:
+                        cls, doc = os.path.split(os.path.join(root, file))
+                        _, cls = os.path.split(cls)
+                        if cls in self.articles:
+                            self.articles[cls].append(doc)
+                        else:
+                            self.articles[cls] = []
+                            self.articles[cls].append(doc)
+        except FileNotFoundError as e:
+            raise e
 
 
 if __name__ == "__main__":
-    a = BlogMaker()
-    print(a.password)
-    print(a.path)
-    a.genArticles()
+    # a = BlogMaker()
+    # print(a.password)
+    # print(a.path)
+    # a.genArticles()
+    a = Article("$test@topic%2023-10-01#test1#test2")
+    a.generate(os.path.join(os.getcwd(), 'index.html'))
+    print(a.toJson())
--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-import yaml
 import os
-# 打开并读取YAML文件
-print(os.getcwd())
-with open('config.yaml', 'r', encoding='utf8') as file:
-    data = yaml.safe_load(file)
+import time
 
-# 打印解析后的数据
-print(data['md-path'])
+# 文件路径
+file_path = "run.sh"
+
+# 获取最后修改时间（时间戳）
+modification_time = os.path.getmtime(file_path)
+
+# 转换为可读格式
+readable_time = time.ctime(modification_time)
+print(f"文件最后修改时间: {readable_time}")
+
+# 或者使用 datetime 模块
+from datetime import datetime
+readable_time = datetime.fromtimestamp(modification_time)
+print(f"文件最后修改时间: {readable_time}")
